76fbb802ce3a3aec4645a13b5c897145bf26d87c
Matias Leandro Lucena <lucenamatias039@gmail.com>
2026-03-02 13:27:53 -0300

    j

--- js/19-detalleDelProducto.js
+++ js/19-detalleDelProducto.js
@@ -1,3 +1,25 @@
-const idProducto = new URLSearchParams(location.search).get("id")
-document.writeln(idProducto)
-//clase 23 de febrero 10 minutos
+(
+    async()=>{
+    const divProductos = document.getElementById("divFilaProducto")
+    const idProducto = new URLSearchParams(location.search).get("id")//con este buscamos el id
+    const productoFakeStore = await fetch(`https://fakestoreapi.com/products/${idProducto}`)//con este traemos el producto
+    const objetoDelProducto = await productoFakeStore.json()//aqui lo convertimos en objeto
+    console.log(objetoDelProducto)    
+
+    divProductos.innerHTML = `
+
+    <div class="col-12 col-md-6">
+        <img src="${objetoDelProducto.image}" alt="">
+      </div>
+      <div class="col-12 col-md-6">
+        <p>$${objetoDelProducto.price}</p>
+        <p>${objetoDelProducto.description}</p>    
+        <button class="btn btn-success">Añadir al carrito</button>
+        <button class="btn btn-danger">Añadir a favoritos</button>
+      </div>
+    `
+
+
+    }
+)()
+//clase 23 de febrero 24minutos
