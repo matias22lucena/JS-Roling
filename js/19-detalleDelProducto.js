fba352c7aa042719da950bec656752f51991d636
matias22lucena <matias22lucena@gmail.com>
2026-03-03 00:04:35 -0300

    s

--- js/19-detalleDelProducto.js
+++ js/19-detalleDelProducto.js
@@ -1,25 +1,31 @@
 (
-    async()=>{
+  async () => {
     const divProductos = document.getElementById("divFilaProducto")
+    const tituloProducto = document.getElementById("tituloProducto")
     const idProducto = new URLSearchParams(location.search).get("id")//con este buscamos el id
     const productoFakeStore = await fetch(`https://fakestoreapi.com/products/${idProducto}`)//con este traemos el producto
     const objetoDelProducto = await productoFakeStore.json()//aqui lo convertimos en objeto
-    console.log(objetoDelProducto)    
-
+    console.log(objetoDelProducto)
+    tituloProducto.textContent = `${objetoDelProducto.title}`
     divProductos.innerHTML = `
 
     <div class="col-12 col-md-6  div-imagen  text-center">
         <img src="${objetoDelProducto.image}" alt="">
       </div>
       <div class="col-12 col-md-6">
-        <p>$${objetoDelProducto.price}</p>
-        <p>${objetoDelProducto.description}</p>    
+
+        <h3>$${objetoDelProducto.price}</h3>
+        <div class="w-75">
+        <p>${objetoDelProducto.description}</p>
+        </div>
+        <div class="w-75 text-center">      
         <button class="btn btn-success">Añadir al carrito</button>
         <button class="btn btn-danger">Añadir a favoritos</button>
+        </div>
       </div>
     `
 
 
-    }
+  }
 )()
-//clase 23 de febrero 27minutosss
+//clase 23 de febrero 43minutosss
