2cd0ad65b33bbe47ba11a6ad3ceeeffb56ccf20e
matias22lucena <matias22lucena@gmail.com>
2026-03-03 10:57:14 -0300

    a

--- js/19-detalleDelProducto.js
+++ js/19-detalleDelProducto.js
@@ -19,13 +19,14 @@
         <p>${objetoDelProducto.description}</p>
         </div>
         <div class="w-75 text-center">      
-        <button class="btn btn-success">Añadir al carrito</button>
-        <button class="btn btn-danger">Añadir a favoritos</button>
+        <button class="btn btn-success mx-2" id="botonAgregarProductoAlCarrito">Añadir al carrito</button>
+        <button class="btn btn-danger mx-2">Añadir a favoritos</button>
         </div>
       </div>
     `
-
-
+    //esto asi no nos toma porque esta siendo creado desde js. lo q tenemos q hacer es crear un evento desde html (que seria el ON ya sea onclick etc.  ese click invoca una funcion q se la crea en aqui en js)
+    const botonCarrito = document.getElementById("botonAgregarProductoAlCarrito")
+    console.log(botonCarrito)
   }
 )()
-//clase 23 de febrero 43minutosss
+//clase 23 de febrero 49minutosss
